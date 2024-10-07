f0ed889f7934a7d9a5a72ca7ffcec8e7e95bd03a
adlerpagliarini <adlerpagliarini@gmail.com>
2024-10-07 01:00:31 -0300

    64. Minimum Path Sum

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Leetcode.Solutions;
+using static Leetcode.Solutions.Lc_64_MinimumPathSum;
 
 Console.WriteLine("Adler Pagliarini, Leetcode Solutions!");
 Lc_200_Tests.Execute();
@@ -11,3 +12,4 @@ Lc_797_Tests.Execute();
 Lc_21_Tests.Execute();
 Lc_210_Tests.Execute();
 Lc_62_Tests.Execute();
+Lc_64_Tests.Execute();
---
+++ Leetcode/Solutions/Lc_64_MinimumPathSum.cs
@@ -0,0 +1,42 @@
+﻿namespace Leetcode.Solutions
+{
+    public class Lc_64_MinimumPathSum
+    {
+        // 64. Minimum Path Sum
+        // https://leetcode.com/problems/minimum-path-sum/description/
+        public int MinPathSum(int[][] grid)
+        {
+            var positionsToSum = new List<(int x, int y)>() { (-1, 0), (0, -1) }; // above, left
+            for (int i = 0; i < grid.Length; i++)
+                for (int j = 0; j < grid[0].Length; j++)
+                {
+                    if (i == 0 && j == 0)
+                        grid[0][0] = grid[0][0];
+                    else
+                    {
+                        var neighbors = new List<int>();
+                        foreach (var moveTo in positionsToSum)
+                        {
+                            var x = moveTo.x + i;
+                            var y = moveTo.y + j;
+                            if (x < 0 || y < 0) continue;
+                            neighbors.Add(grid[x][y]);
+                        }
+                        grid[i][j] = neighbors.Min() + grid[i][j];
+                    }
+                }
+            return grid[grid.Length - 1][grid[0].Length - 1];
+        }
+        public static class Lc_64_Tests
+        {
+            public static void Execute()
+            {
+                Console.WriteLine("Leetcode: 64 Unique Paths");
+
+                var lc64code = new Lc_64_MinimumPathSum();
+                Console.WriteLine("Expected Output: 7 - " + lc64code.MinPathSum([[1, 3, 1], [1, 5, 1], [4, 2, 1]]));
+                Console.WriteLine("");
+            }
+        }
+    }
+}
